0f6e3d2749c120ae9754bc3fc7981cffb8d32d69
seunghy1468 <47622991+seunghy1468@users.noreply.github.com>
2025-08-06 11:29:16 +0000

    Deploying to gh-pages from @ seunghy1468/seunghy1468.github.io@86066532d78f03cebf6d2168cf1baa74ec1bc1d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -539,13 +539,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/seunghyeong-yoo-83b4a7217/", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
