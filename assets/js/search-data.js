c8a603b19da4037dbd060659e356dd4175770f1f
seunghy1468 <47622991+seunghy1468@users.noreply.github.com>
2025-08-06 08:10:09 +0000

    Deploying to gh-pages from @ seunghy1468/seunghy1468.github.io@2f6358ee782dc38c4317dd8fa74729ba4b13c9e1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-seunghyeong-yoo",
-    title: "Seunghyeong Yoo",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
